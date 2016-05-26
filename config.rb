3ab64411e4304fafceecc22f39d91ff6dbf2971a
laltuHIPL <laltu.sardar@careclues.com>
2016-05-26 14:49:07 +0530

    migaration file added

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'require_all'
 enable :partial
 enable :sessions
-DB = Sequel.connect('mysql://root:satyaisical@localhost/drug_management')
+DB = Sequel.connect('mysql://root:hiplsql@localhost/drug_management')
 Sequel::Model.plugin :validation_helpers
 
 require_all 'controller/**/*.rb'
