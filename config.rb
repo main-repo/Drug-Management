c38c5dff7bf57c0b0c3e0d95024118aefc80b9d5
laltuHIPL <laltu.sardar@careclues.com>
2016-05-27 14:29:57 +0530

    config file added, other changes accordingly

--- config.rb
+++ config.rb
@@ -1,7 +1,10 @@
 require 'require_all'
+require 'sinatra/config_file'
+config_file 'config.yml'
+
 enable :partial
 enable :sessions
-DB = Sequel.connect('mysql://root:hiplsql@localhost/drug_management')
+Sequel.connect(settings.db)
 Sequel::Model.plugin :validation_helpers
 
 require_all 'controller/**/*.rb'
