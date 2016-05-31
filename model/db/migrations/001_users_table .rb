d3e418e1f0bda9157f33eb57710cff0b8c19727d
satishisical <satish.chandra@careclues.com>
2016-05-31 12:48:12 +0530

    comit

---
+++ model/db/migrations/001_users_table .rb
@@ -0,0 +1,33 @@
+Sequel.migration do
+  change do
+    create_table(:users) do
+      primary_key :id
+      String :email
+      String :name
+      String :mobile ,:size=>10
+      String :password ,:size=>64
+    end
+
+    create_table :sales do
+  	  primary_key :id
+  	  integer :order_id
+  	  String :name 
+  	  integer :quantity,:size=>10
+  	  integer :price
+    end
+
+    create_table :categories do
+      primary_key :id
+      String :name
+    end
+
+    create_table :drugs do
+      primary_key :id
+      String :name
+      integer :quantity,:size=>10
+      integer :price,:size=>8
+    end
+
+    create_join_table(:drug_id=>:drugs, :category_id=>:categories)
+  end
+end
---
+++ models/categories_drugs.rb
@@ -0,0 +1,3 @@
+class Category_drug < Sequel::Model(:categories_drugs)
+ 	unrestrict_primary_key
+end
