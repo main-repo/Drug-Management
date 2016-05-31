347872aa91d861f0680a2d5a87096096b582b2d3
satishisical <satish.chandra@careclues.com>
2016-05-31 17:05:12 +0530

    comit

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,6 @@ function editDrug(drug) {
   console.log(drug.parentNode.parentNode.children[1])
   // alert("Drug");
 }
+function add_drugs(drug){
+	console.log(drug.parentNode.parentNode.children[1])
+}
